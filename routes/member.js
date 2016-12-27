ec8c36a60ef227d122a1164c10ce5e40e7818783
hayyyy <yheea403@naver.com>
2016-12-27 16:28:23 +0900

    회원가입 관련 페이지들 전꺼에서 가져옴

---
+++ routes/member.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+//var mysql = require("./model/mysql");
+
+router.get('/join_step_1', function(req, res, next) {
+
+	res.render('front/cid_member/cid_join_step_1', { });
+
+});
+
+router.get('/join_step_2', function(req, res, next) {
+
+	res.render('front/cid_member/cid_join_step_2', { });
+
+});
+
+router.get('/join_step_3', function(req, res, next) {
+
+	res.render('front/cid_member/cid_join_step_3', { });
+
+});
+
+router.get('/join', function(req, res, next) {
+
+	res.render('front/cid_member/cid_join', { });
+
+});
+
+router.get('/login', function(req, res, next) {
+
+	res.render('front/cid_member/cid_login', { });
+
+});
+module.exports = router;
