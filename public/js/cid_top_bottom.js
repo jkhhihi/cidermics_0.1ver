1ae5de9563b25fbf75cb67efb7016cf3b6f55749
hayyyy <yheea403@naver.com>
2017-05-30 17:21:22 +0900

    *5월30일 오후2

--- public/js/cid_top_bottom.js
+++ public/js/cid_top_bottom.js
@@ -1,25 +1,25 @@
 // JavaScript Document
-$(document).ready(function(){
+$(document).ready(function(){	
 	
-$(".mob-menu").click(function() {
-  $("#menu").addClass("open");
-});
-
-$(".close").click(function() {
-  $("#menu").removeClass("open");
-});
-
-$(".mob-menu").click(function () {
-    $("#menu,.page_cover,html").addClass("open");
-    window.location.hash = "#open"; 
-});
-
-window.onhashchange = function () {
-    if (location.hash != "#open") { 
-        $("#menu,.page_cover,html").removeClass("open");
-    }
-};
-
+	$(".mob-menu").click(function() {
+	  $("#menu").addClass("open");
+	});
+	
+	$(".close").click(function() {
+	  $("#menu").removeClass("open");
+	});
+	
+	$(".mob-menu").click(function () {
+	    $("#menu,.page_cover,html").addClass("open");
+	    window.location.hash = "#open"; 
+	});
+	
+	window.onhashchange = function () {
+	    if (location.hash != "#open") { 
+	        $("#menu,.page_cover,html").removeClass("open");
+	    }
+	};
+	
 });
 
 
