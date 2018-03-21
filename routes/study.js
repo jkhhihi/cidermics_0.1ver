3f83b15f6ab2e505a1d84ea8ec5e66b4fa14d9e4
user <user@DESKTOP-J4EN58S>
2018-03-21 21:13:15 +0900

    *3월21일1

--- routes/study.js
+++ routes/study.js
@@ -67,7 +67,7 @@ router.get('/studymore/:idx', function(req, res, next) {
 router.get('/studymore1/:idx', function(req, res, next) {
 	var idx = req.params.idx;
 	var stdlist;
-	mysql.select('SELECT * from cider.std_more where idx = '+idx+';', function (err, data){
+	mysql.select('SELECT idx,cate,subject,bgimg1,thum2,leader,period,sche1,sche2,sche3,location,people,price,img1,img2,img3 from cider.std_more where idx = '+idx+';', function (err, data){
 		mysql.select('SELECT idx,subject,subject2,decate,recentdate,thum,leader,sche1 from cider.std_more where flag="Y" order by idx desc;', function (err, data1){
 	res.render('front/cid_study/std_more1', {md:data, stdlist : data1});
   	});
